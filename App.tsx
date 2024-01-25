a76b23cf824cdd3ee523bb49abb9e1c9890b4340
Mark Madlangbayan <5464773+markmadlangbayan@users.noreply.github.com>
2024-01-25 09:58:50 -0600

    Made it easier to read

--- App.tsx
+++ App.tsx
@@ -1,19 +1,25 @@
 import { useState, memo } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
-const Memo2 = memo(({i}: {i: () => string}) => {
+type Memo2Props = {
+  getName: () => string
+}
+
+const Memo2 = (props: Memo2Props) => {
+
   console.log('Memo2 got rendered!');
-  return <Text>Hi {i()}</Text>;
+  return <Text>Hi {props.getName()}</Text>;
+
+};
+
+const Memo1 = memo(() => {
+  console.log('Memo1 got rendered!');
+  return <Text>Memo1</Text>;
 });
 
 
 export default function App() {
   const [num1, setNum1] = useState(0);
-
-  const Memo1 = memo(() => {
-    console.log('Memo1 got rendered!');
-    return <Text>Memo1</Text>;
-  });
 
   return (
     <View style={styles.container}>
@@ -22,7 +28,7 @@ export default function App() {
         <Memo1 />
       </View>
 
-      <Memo2 i={() => 'Baylor'} />
+      <Memo2 getName={() => 'Baylor'} />
     </View>
   );
 }
