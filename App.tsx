644613a8ed99d1ca2788e9bc26ab850cb8f13869
Mark Madlangbayan <5464773+markmadlangbayan@users.noreply.github.com>
2024-01-25 19:43:08 -0600

    Fixed issue

--- App.tsx
+++ App.tsx
@@ -1,27 +1,29 @@
 import { useState, memo } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
-type Memo2Props = {
+type GreetingLabelProps = {
   getName: () => string
 }
 
-const GreetingLabel = ({getName}: Memo2Props) => {
+const GreetingLabel = memo(({getName}: GreetingLabelProps) => {
   console.log('GreetingLabel got rendered!');
   return <Text>Hi {getName()}</Text>;
-};
+});
+
+const MemoLabel = memo(() => {
+  console.log('MemoLabel got rendered!');
+  return <Text>Memo</Text>;
+});
+
+const getName = () => 'Baylor';
 
 export default function App() {
   const [num1, setNum1] = useState(0);
 
-  const MemoLabel = memo(() => {
-    console.log('MemoLabel got rendered!');
-    return <Text>Memo</Text>;
-  });
-
   return (
     <View style={s.container}>
       <Button title="Tap" onPress={() => setNum1(num1 + 1)}/>
-      <GreetingLabel getName={() => 'Baylor'} />
+      <GreetingLabel getName={getName} />
 
       <MemoLabel />
     </View>
